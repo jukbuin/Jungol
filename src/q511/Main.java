83cb25dd58f683d130567831c22fba3cb8c6594e
Class01 <Class01@DESKTOP-QKQPSM5>
2023-04-26 17:40:30 +0900

    정올 511문제

---
+++ src/q511/Main.java
@@ -0,0 +1,17 @@
+package q511;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 55;
+		int b = 10;
+		
+		System.out.println(a + " - " + b + " = " + (a-b));
+		
+		int c = 2008;
+		int d = 1999;
+		
+		System.out.println(c + " - " + d + " = " + (c-d));
+	}
+
+}
