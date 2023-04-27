bc66a6ff512a26574b74afd8f00b104276f07036
Class01 <Class01@DESKTOP-QKQPSM5>
2023-04-27 12:13:40 +0900

    정올 508번문제

---
+++ src/q508/Main.java
@@ -0,0 +1,18 @@
+package q508;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		System.out.println("      item     count     price");
+//		System.out.println("       pen        20       100");
+//		System.out.println("      note         5        95");
+//		System.out.println("    eraser       110        97");
+		
+		System.out.printf("%10s%10s%10s\n", "item", "count", "price");
+		System.out.printf("%10s%10d%10d\n", "pen", 20, 100);
+		System.out.printf("%10s%10d%10d\n", "note", 5, 95);
+		System.out.printf("%10s%10d%10d\n", "eraser", 110, 97);
+		
+	}
+
+}
