82f444cdd07b7384a9a6a6e1dc6c9ad12056525c
Class01 <Class01@DESKTOP-QKQPSM5>
2023-04-27 17:46:38 +0900

    정올 107번문제

--- src/q107/Main.java
+++ src/q107/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		double num1 = 80.5;
 		double num2 = 22.34;
 		
-		System.out.printf("%10.2f %9.2f %9.2f", num1, num2, num1+num2);
+		System.out.printf("%10.2f%10.2f%10.2f", num1, num2, num1+num2);
 	}
 
 }
