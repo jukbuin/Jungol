bc4809dc78b64b0dd0739340042ce3e4781e691d
Class01 <Class01@DESKTOP-QKQPSM5>
2023-04-27 17:38:02 +0900

    정올 106번문제

---
+++ src/q106/Main.java
@@ -0,0 +1,13 @@
+package q106;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = 30;
+		System.out.println(a + " + " + b + " = " + c);
+		System.out.printf("%d + %d = %d", a, b, c);
+	}
+
+}
