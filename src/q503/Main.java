5c4d46f03b68de3d6412be20f61cc6db9e1aaa13
Class01 <Class01@DESKTOP-QKQPSM5>
2023-04-27 09:50:56 +0900

    정올 503번문제

--- src/q503/Main.java
+++ src/q503/Main.java
@@ -3,7 +3,7 @@ package q503;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("My name is Hong Gil Dong.\r\n"
+		System.out.println("My name is Hong Gil Dong.\n"
 				+ "I am 13 years old.");
 	}
 
