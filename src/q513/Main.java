443c7e3e7f6ba1f445c19198c773a01fe644e97f
Class01 <Class01@DESKTOP-QKQPSM5>
2023-04-27 17:01:10 +0900

    정올 입력 513번문제

---
+++ src/q513/Main.java
@@ -0,0 +1,18 @@
+package q513;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double yd = 91.44;
+		double in = 2.54;
+		
+		double result1 = yd * 2.1;
+		double result2 = in * 10.5;
+		
+		System.out.printf("%4.1fyd = %5.1fcm\n", 2.1, result1);
+		System.out.printf("%4.1fin = %5.1fcm", 10.5, result2);
+		
+		
+	}
+
+}
