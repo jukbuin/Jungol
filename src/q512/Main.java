c8cc81d2e702b4c7317fab079239c945ba64c47b
Class01 <Class01@DESKTOP-QKQPSM5>
2023-04-27 14:14:55 +0900

    정올 512번 문제

---
+++ src/q512/Main.java
@@ -0,0 +1,16 @@
+package q512;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int Weight = 49;
+		double Gravity = 0.2683;
+//		
+//		System.out.print(Weight + " * ");
+//		System.out.printf("%f", Gravity);
+//		System.out.print(" = ");
+//		System.out.printf("%f", (Weight*Gravity));
+		System.out.printf("%d * %f = %f", Weight, Gravity, (Weight*Gravity));
+	}
+
+}
