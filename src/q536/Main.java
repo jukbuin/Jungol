efa5cc4c711901e8ae028ea7c6c15ec3eb7a0eba
Class01 <Class01@DESKTOP-QKQPSM5>
2023-04-28 18:04:11 +0900

    정올 6.536번문제

--- src/q536/Main.java
+++ src/q536/Main.java
@@ -4,13 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		int i = 1;
+//		1. while문
 //		while (i <= 15) {
 //			System.out.print(i + " ");
 //			i++;
 //		}
+//		2. for문
 //		for(int i = 1; i <= 15; i++) {
 //			System.out.print(i + " ");
 //		}
+//		3. do-while문
 		do {
 			System.out.print(i++ + " ");
 		} while (i <= 15);
