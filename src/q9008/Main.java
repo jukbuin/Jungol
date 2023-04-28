87a2aca179d47c57c9d887aa9569ac1b03d811e9
Class01 <Class01@DESKTOP-QKQPSM5>
2023-04-28 09:40:36 +0900

    정올 9008번문제

---
+++ src/q9008/Main.java
@@ -0,0 +1,10 @@
+package q9008;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(
+				" subject score\n" + "==============\n" + "  korean    90\n" + " english   100\n" + "computer    80");
+	}
+
+}
