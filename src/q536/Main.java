948859a3cae69f4b566fdd7abce57b97906fb116
Class01 <Class01@DESKTOP-QKQPSM5>
2023-04-28 17:44:17 +0900

    정올 6.536번문제

---
+++ src/q536/Main.java
@@ -0,0 +1,19 @@
+package q536;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i = 1;
+//		while (i <= 15) {
+//			System.out.print(i + " ");
+//			i++;
+//		}
+//		for(int i = 1; i <= 15; i++) {
+//			System.out.print(i + " ");
+//		}
+		do {
+			System.out.print(i++ + " ");
+		} while (i <= 15);
+	}
+
+}
