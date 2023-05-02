7d7d491b90292e40457c65e9213a20b2335d98b5
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-02 14:19:56 +0900

    배열 연습문제~

---
+++ src/q0000/Main.java
@@ -0,0 +1,20 @@
+package q0000;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		double b = sc.nextDouble();
+		String c = sc.next();
+		char d = sc.next().charAt(0);
+		sc.close();
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+	}
+
+}
