2588ac1a54e555c87e13bd2f07f32d082157bff7
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-03 17:53:16 +0900

    정올 14.594번문제

---
+++ src/q594/Main.java
@@ -0,0 +1,29 @@
+package q594;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		sc.close();
+		
+		System.out.print(a + a);
+		
+//		10번 반복하기
+//      1.for문
+//		for (int i = 0; i < 10; i++) {
+//			System.out.print(a);
+//		}
+
+//		2.while문
+//		int i = 0;
+//		while (i < 10) {
+//			System.out.print(a);
+//			i++;
+//		}
+
+	
+	}
+}
