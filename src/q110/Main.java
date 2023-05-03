d9b7ecbbc858d4407edc93408d0319652aff79e4
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-03 14:58:11 +0900

    정올 2.110번문제

--- src/q110/Main.java
+++ src/q110/Main.java
@@ -9,6 +9,7 @@ public class Main {
 		System.out.print("yard? ");
 		double yard = sc.nextDouble();
 		sc.close();
+		
 		System.out.print(yard + "yard = ");
 		System.out.printf("%.1f%s", (yard * 91.44), "cm");
 	}
