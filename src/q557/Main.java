7d92631229a0b25e6ea70dbacb7d67d16ed6ff26
h0409 <h0409@192.168.55.109>
2023-05-07 15:33:01 +0900

    정올 9.557번문제

---
+++ src/q557/Main.java
@@ -0,0 +1,18 @@
+package q557;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i = 0;
+		char[] al = new char[10];
+		for (i = 0; i < 10; i++) {
+			al[i] = sc.next().charAt(0);
+		}
+		sc.close();
+		System.out.printf("%c %c %c", al[0], al[3], al[6]);
+	}
+
+}
