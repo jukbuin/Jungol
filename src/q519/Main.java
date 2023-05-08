c3f1b046ead3760db1975a818357a56cf66f0423
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-08 09:43:22 +0900

    정올 3.519번문제

--- src/q519/Main.java
+++ src/q519/Main.java
@@ -9,9 +9,9 @@ public class Main {
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		sc.close();
-		
+
 		int c = a + 100;
-		int d = b%10;
+		int d = b % 10;
 		System.out.print(c + " ");
 		System.out.print(d);
 	}
