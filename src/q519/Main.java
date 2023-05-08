41d10f7f81d84144b9b23cdc97163812d958a9a3
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-08 10:17:16 +0900

    정올 3.519번문제 버퍼리더기를 써보았다~변수 변경 어뜨케해..?

--- src/q519/Main.java
+++ src/q519/Main.java
@@ -1,21 +1,32 @@
 package q519;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		sc.close();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		String s1 = st.nextToken();
+		String s2 = st.nextToken();
 
-		a += 100;
-		b %= 10;
-//		int c = a + 100;
-//		int d = b % 10;
-		System.out.print(a + " ");
-		System.out.print(b);
+		System.out.print(s1 + " ");
+		System.out.print(s2);
+
+//		Scanner sc = new Scanner(System.in);
+//		int a = sc.nextInt();
+//		int b = sc.nextInt();
+//		sc.close();
+//
+//		a += 100;
+//		b %= 10;
+////		int c = a + 100;
+////		int d = b % 10;
+//		System.out.print(a + " ");
+//		System.out.print(b);
 	}
 
 }
