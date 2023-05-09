97d8ccd99a18ea5bfab31f0c162f6ece2649b367
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-09 16:16:20 +0900

    정올 2.9012번문제

---
+++ src/q9012/Main.java
@@ -0,0 +1,10 @@
+package q9012;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.printf("원주 = %d * %d * %.6f = %.6f\n", 5, 2, 3.140000, 31.40000);
+		System.out.printf("넓이 = %d * %d * %.6f = %.6f\n", 5, 5, 3.140000, 78.50000);
+	}
+
+}
