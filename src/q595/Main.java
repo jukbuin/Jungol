6f91a9bf2e96af209dc62c27bad68fe9a37df4c3
h0409 <h0409@192.168.55.109>
2023-05-09 22:12:15 +0900

    정올 14.595번문제

---
+++ src/q595/Main.java
@@ -0,0 +1,12 @@
+package q595;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String str = "Hong Gil Dong";
+		for (int i = 3 ; i > 2 && i < 7;i++) {
+			System.out.print(str.charAt(i));
+		}
+	}
+
+}
