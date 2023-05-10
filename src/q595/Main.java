a50efed97a9fa1ccbc90b9a81ee95110cb5dd14d
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-10 15:43:40 +0900

    정올 14.595번문제

--- src/q595/Main.java
+++ src/q595/Main.java
@@ -4,9 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		String str = "Hong Gil Dong";
-		for (int i = 3 ; i > 2 && i < 7;i++) {
+		for (int i = 3 ;i < 7;i++) {
 			System.out.print(str.charAt(i));
 		}
+//		System.out.println();
+//		System.out.print(str.substring(3, 7)); 
+//		*substring = 문자열 3번인덱스부터 7번인덱스전까지
 	}
 
 }
