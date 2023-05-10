168ebde22100c5c468a1c81c4d18f1737943beb7
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-10 09:49:05 +0900

    1.9005번문제

---
+++ src/q9005/Main.java
@@ -0,0 +1,12 @@
+package q9005;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String a = "서식 문자로 출력하는 방법입니다.";
+		String b = "이것은 문자열을 나타냅니다.";
+		System.out.printf("%s\n", a);
+		System.out.printf("%s\n", b);
+	}
+
+}
