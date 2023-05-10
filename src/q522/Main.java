e21f314ba81bd010127430f86b0dc1f9099335e6
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-10 18:08:52 +0900

    정올 3.522번문제

--- src/q522/Main.java
+++ src/q522/Main.java
@@ -9,17 +9,24 @@ public class Main {
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		sc.close();
+
+//		if (a == b) {
+//			System.out.println(1);
+//		} else {
+//			System.out.println(0);
+//		}
+//		if (a != b) {
+//			System.out.println(1);
+//		} else {
+//			System.out.println(0);
+//		}
 		
-		if (a == b) {
-			System.out.println(1);
-		} else {
-			System.out.println(0);
-		}
-		if (a != b) {
-			System.out.println(1);
-		} else {
-			System.out.println(0);
-		}
+		int c = (a == b) ? 1 : 0;
+		System.out.println(c);
+		int d = !(a == b) ? 1 : 0;
+		System.out.println(d);
+		
+
 	}
 
 }
