2703f4a6cc28e25842eb078b6624f11bfe4321a4
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-10 09:42:19 +0900

    정올 1.504번문제

---
+++ src/q504/Main.java
@@ -0,0 +1,11 @@
+package q504;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("(@) (@)");
+		System.out.println("(=^.^=)");
+		System.out.println("(-m-m-)");
+	}
+
+}
