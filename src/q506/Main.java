c23e59e63c0404b8c8e2be8fa4138c475b491ca2
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-11 09:58:09 +0900

    정올 1.506번문제

---
+++ src/q506/Main.java
@@ -0,0 +1,12 @@
+package q506;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int height = 170;
+		double weight = 68.600000;
+		System.out.println("My height\n" + height);
+		System.out.printf("My weight\n%.6f", weight);
+	}
+
+}
