a1961797ef7673abb04986251f88c1af6accf8b7
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-11 09:58:00 +0900

    정올 1.505번문제

---
+++ src/q505/Main.java
@@ -0,0 +1,12 @@
+package q505;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String a = "I can program well.";
+		String b = "Dreams come true.";
+		
+		System.out.printf("%s\n%s", a, b);
+	}
+
+}
