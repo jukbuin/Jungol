120db826b8140268d58d947b2859c3c2926a7ca2
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-11 10:32:09 +0900

    정올 1.103번문제

---
+++ src/q103/Main.java
@@ -0,0 +1,13 @@
+package q103;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("TTTTTTTTTT");
+		System.out.println("TTTTTTTTTT");
+		System.out.println("    TT    ");
+		System.out.println("    TT    ");
+		System.out.println("    TT    ");
+	}
+
+}
