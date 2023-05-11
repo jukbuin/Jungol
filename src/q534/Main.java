320d8f6b7410f4069e7f9db31943cefae7988577
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-11 13:55:35 +0900

    정올 5.534번문제

--- src/q534/Main.java
+++ src/q534/Main.java
@@ -8,6 +8,8 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		char al = sc.next().charAt(0);
 		sc.close();
+		
+//		1.switch문
 
 		switch (al) {
 		case 'A':
@@ -28,6 +30,22 @@ public class Main {
 		default:
 			System.out.print("error");
 		}
+		
+//		2.if~else문
+		
+//		if (al == 'A') {
+//			System.out.print("Excellent");
+//		} else if (al == 'B') {
+//			System.out.print("Good");
+//		} else if (al == 'C') {
+//			System.out.print("Usually");
+//		} else if (al == 'D') {
+//			System.out.print("Effort");
+//		} else if (al == 'F') {
+//			System.out.print("Failure");
+//		} else {
+//			System.out.print("error");
+//		}
 	}
 
 }
