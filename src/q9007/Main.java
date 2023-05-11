df524126841a7cb5258510600f5e180156ecd45b
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-11 10:12:21 +0900

    정올 1.9007번문제

---
+++ src/q9007/Main.java
@@ -0,0 +1,11 @@
+package q9007;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("수식을 출력하면 계산 결과가 출력됩니다.");
+		System.out.println(10 + " + " + 5 + " = " + (10+5));
+		System.out.println("내 생일은 2005년 6월 17일 입니다.");
+	}
+
+}
