d4787807faae5e7554cebe3fc4e5994c5d967c0a
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-12 09:55:49 +0900

    정올 1.105번문제

---
+++ src/q105/Main.java
@@ -0,0 +1,32 @@
+package q105;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String a = "Seoul";
+		String b = "10,312,545";
+		String c = "+91,375";
+		System.out.printf("%15s%15s%15s\n", a, b, c);
+
+		String d = "Pusan";
+		String e = "3,567,910";
+		String f = "+5,868";
+		System.out.printf("%15s%15s%15s\n", d, e, f);
+
+		String g = "Incheon";
+		String h = "2,758,296";
+		String i = "+64,888";
+		System.out.printf("%15s%15s%15s\n", g, h, i);
+
+		String j = "Daegu";
+		String k = "2,511,676";
+		String l = "+17,230";
+		System.out.printf("%15s%15s%15s\n", j, k, l);
+
+		String m = "Gwangju";
+		String n = "1,454,636";
+		String o = "+29,774";
+		System.out.printf("%15s%15s%15s\n", m, n, o);
+	}
+
+}
