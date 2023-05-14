76123298ab228da61672a2d155e4b09c46d488ae
h0409 <h0409@192.168.55.109>
2023-05-14 11:14:36 +0900

    정올 3.523번문제

---
+++ src/q523/Main.java
@@ -0,0 +1,31 @@
+package q523;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		sc.close();
+
+		if (num1 > num2) {
+			System.out.println(num1 + " > " + num2 + " --- " + true);
+			System.out.println(num1 + " < " + num2 + " --- " + false);
+			System.out.println(num1 + " >= " + num2 + " --- " + true);
+			System.out.println(num1 + " <= " + num2 + " --- " + false);
+		} else if (num1 < num2) {
+			System.out.println(num1 + " > " + num2 + " --- " + false);
+			System.out.println(num1 + " < " + num2 + " --- " + true);
+			System.out.println(num1 + " >= " + num2 + " --- " + false);
+			System.out.println(num1 + " <= " + num2 + " --- " + true);
+		} else if (num1 == num2) {
+			System.out.println(num1 + " > " + num2 + " --- " + false);
+			System.out.println(num1 + " < " + num2 + " --- " + false);
+			System.out.println(num1 + " >= " + num2 + " --- " + true);
+			System.out.println(num1 + " <= " + num2 + " --- " + true);
+		}
+	}
+
+}
