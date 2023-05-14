d1588369b6ce5561bc17fe12f456f706a3ae522d
h0409 <h0409@192.168.55.109>
2023-05-14 12:12:12 +0900

    정올 5.632번문제

---
+++ src/q632/Main.java
@@ -0,0 +1,22 @@
+package q632;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		sc.close();
+
+		int min = 0;
+		min = (a < b) ? a : b;
+		min = (min < c) ? min : c;
+
+		System.out.print(min);
+
+	}
+
+}
