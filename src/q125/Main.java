f3f7d21529683bb8f60ce86b07666d79a0a09538
h0409 <h0409@192.168.55.109>
2023-05-14 15:18:58 +0900

    정올 6.125번문제

---
+++ src/q125/Main.java
@@ -0,0 +1,17 @@
+package q125;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		sc.close();
+
+		for (int i = 1; i <= num; i++) {
+			System.out.print(i + " ");
+		}
+	}
+
+}
