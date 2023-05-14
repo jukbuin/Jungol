21bab62bdb9887ee33fb3f52d784659e0f63caa1
h0409 <h0409@192.168.55.109>
2023-05-14 15:52:26 +0900

    정올 9.560번문제

---
+++ src/q560/Main.java
@@ -0,0 +1,22 @@
+package q560;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a[] = new int[10];
+		int i = 0;
+		int min = 1000;
+		for (i = 0; i < a.length; i++) {
+			a[i] = sc.nextInt();
+		}
+		for (i = 0; i < a.length; i++) {
+			min = (min < a[i]) ? min : a[i];
+		}
+		System.out.print(min);
+		sc.close();
+	}
+
+}
