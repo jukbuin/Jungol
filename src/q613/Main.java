058f68d11fc43409540c8fee344395e5f43a4146
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-15 18:19:42 +0900

    정올 16.613번문제

--- src/q613/Main.java
+++ src/q613/Main.java
@@ -4,9 +4,20 @@ import java.util.Scanner;
 
 class School {
 	String name;
-	String sch;
+	String school;
 	int grade;
+
 	
+	School() {
+		
+	}
+	
+	School(String n, String s, int g){
+		name = n;
+		school = s;
+		grade = g;
+
+	}
 }
 
 public class Main {
@@ -14,34 +25,34 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		School s1 = new School();
+
 
 		String name = sc.next();
 		String school = sc.next();
-		int grade = sc.nextInt();
+		int grade = sc.nextInt();	
+//		생성자 이용
+		School s1 = new School(name, school,grade);
 		
-		s1.name = name;
-		s1.sch = school;
-		s1.grade = grade;
+		name = sc.next();
+		school = sc.next();
+		grade = sc.nextInt();
+		School s2 = new School(name, school,grade);
 		
-//		School s2 = new School();
-//		name = sc.next();
-//		school = sc.next();
-//		grade = sc.nextInt();
+//		생성자 없이 생성
+//		School s1 = new School();
+//		s1.name = name;
+//		s1.school = school;
+//		s1.grade = grade;
 		
 		sc.close();
-//		
-//		s2.name = name;
-//		s2.sch = school;
-//		s2.grade = grade;
-		
 		
 		System.out.println("Name : " + s1.name);
-		System.out.println("School : " + s1.sch);
+		System.out.println("School : " + s1.school);
 		System.out.println("Grade : " + s1.grade);
-//		System.out.println("Nmae : " + s2.name);
-//		System.out.println("School : " + s2.sch);
-//		System.out.println("Grade :  " + s2.grade);
+		System.out.println("Name : " + s2.name);
+		System.out.println("School : " + s2.school);
+		System.out.println("Grade : " + s2.grade);
+
 	}
 
 }
