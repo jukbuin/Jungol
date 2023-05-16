255b60960582439dec28f111f030514071eea50d
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-16 16:57:23 +0900

    정올 14.596번문제

--- src/q596/Main.java
+++ src/q596/Main.java
@@ -6,7 +6,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String 
+		String a = sc.next();
+		int b = sc.nextInt();
+		sc.close();
+		
+		if (a.length() > b) {
+			for (int i = a.length(); i > a.length() - b; i--) {    //(int i = a.lenght()-1; (num > 0 && i >= 0; i--, num--)
+				System.out.print(a.charAt(i - 1));                 // System.out.print(a.charAt(i));
+			}
+		} else {
+			for (int i = a.length(); i > 0; i--) {
+				System.out.print(a.charAt(i - 1));
+			}
+		}
+
 	}
 
 }
