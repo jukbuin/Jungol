306c23cde4164d2e083401825b6cd3242ca896c9
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-16 20:00:52 +0900

    정올 5.120번문제

--- src/q120/Main.java
+++ src/q120/Main.java
@@ -10,11 +10,12 @@ public class Main {
 		int b = sc.nextInt();
 		sc.close();
 
-		if (a >= b) {
-			System.out.print(a - b);
-		} else {
-			System.out.print(b - a);
-		}
+//		if (a >= b) {
+//			System.out.print(a - b);
+//		} else {
+//			System.out.print(b - a);
+//		}
+		System.out.print(a > b ? a - b : b - a);
 	}
 
 }
