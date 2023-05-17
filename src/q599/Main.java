f356447d75c82004ce63f28be6bf0855a477d378
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-17 09:44:34 +0900

    정올 14.599번문제

---
+++ src/q599/Main.java
@@ -0,0 +1,25 @@
+package q599;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String a = sc.next();
+		sc.close();
+
+		if (a.length() <= 100) {
+			for (int i = 0; i < a.length(); i++) {
+				char c = a.charAt(i);
+				if (c >= 'A' && c <= 'Z') {
+					System.out.print((char) c);
+				} else if (c >= 'a' && c <= 'z') {
+					System.out.print((char) (c - 32));
+				}
+			}
+		}
+	}
+
+}
