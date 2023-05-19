b32c97bae67722bef1d3dbbd0bb7d2d7d99923eb
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-19 13:51:25 +0900

    정올 6.127번문제

---
+++ src/q127/Main.java
@@ -0,0 +1,26 @@
+package q127;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int sum = 0;
+		int cnt = 0;
+
+		while (true) {
+			int num = sc.nextInt();
+			if (num < 0 || num > 100) {
+				break;
+			}
+			sum += num;
+			cnt++;
+		}
+		sc.close();
+
+		System.out.println("sum : " + sum);
+		System.out.printf("avg : %.1f", (double) sum / cnt);
+	}
+
+}
