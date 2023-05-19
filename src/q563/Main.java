5b0aeeca6df8eba5ec2c017120482ba3b581b962
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-19 18:06:19 +0900

    정올 9.563번문제

---
+++ src/q563/Main.java
@@ -0,0 +1,15 @@
+package q563;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num[] = new int[10];
+		for (int i = 0; i < num.length; i++) {
+			num[i] = sc.nextInt();
+		}
+		
+	}
+}
