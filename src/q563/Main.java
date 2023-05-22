e17ffa1c7c02d7e4a38fcf6e362e5e273b88d29a
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-22 17:59:46 +0900

    정올 9.563번문제

--- src/q563/Main.java
+++ src/q563/Main.java
@@ -1,15 +1,23 @@
 package q563;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int num[] = new int[10];
+		Integer num[] = new Integer[10];
 		for (int i = 0; i < num.length; i++) {
 			num[i] = sc.nextInt();
 		}
+		sc.close();
 		
+		Arrays.sort(num, Collections.reverseOrder());
+		for (int i = 0; i < num.length; i++) {
+			System.out.print(num[i] + " ");
+		}
+
 	}
 }
