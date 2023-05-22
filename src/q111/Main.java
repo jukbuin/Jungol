faf7f3b7a19aeaa0a238555bc372c85c5323e446
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-22 09:47:56 +0900

    정올 3.111번문제

--- src/q111/Main.java
+++ src/q111/Main.java
@@ -6,13 +6,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		int b = sc.nextInt();
-		int c = sc.nextInt();
-		int d = sc.nextInt();
+//		int a = sc.nextInt();
+//		int b = sc.nextInt();
+//		int c = sc.nextInt();
+//		int d = sc.nextInt();
+		int sum = 0;
+		for(int i = 0; i < 4; i++) {
+			int inp = sc.nextInt();
+			sum += inp;
+		}
 		sc.close();
-
-		int sum = a + b + c + d;
 
 		System.out.println("sum " + sum);
 		System.out.print("avg " + sum / 4);
