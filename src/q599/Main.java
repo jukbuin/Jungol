a7d7525c722268a489d32a5ace28d28180883b25
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-24 13:51:19 +0900

    정올 14.599번문제

--- src/q599/Main.java
+++ src/q599/Main.java
@@ -14,7 +14,7 @@ public class Main {
 			for (int i = 0; i < a.length(); i++) {
 				char c = a.charAt(i);
 				if (c >= 'A' && c <= 'Z') {
-					System.out.print((char) c);
+					System.out.print(c);
 				} else if (c >= 'a' && c <= 'z') {
 					System.out.print((char) (c - 32));
 				}
