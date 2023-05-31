cb6738e86eee4797b2d1dbf2f1c4e0e0a92851c1
Class01 <Class01@DESKTOP-QKQPSM5>
2023-05-31 09:48:48 +0900

    정올 선택제어문 9036번문제

---
+++ src/q9036/Main.java
@@ -0,0 +1,28 @@
+package q9036;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("1. 삽입");
+		System.out.println("2. 수정");
+		System.out.println("3. 삭제");
+		System.out.print("숫자를 선택하세요. ");
+		int num = sc.nextInt();
+
+		switch (num) {
+		case 1:
+			System.out.print("삽입을 선택하셨습니다.");
+			break;
+		case 2:
+			System.out.print("수정을 선택하셨습니다.");
+			break;
+		case 3:
+			System.out.print("삭제을 선택하셨습니다.");
+			break;
+		}
+	}
+
+}
