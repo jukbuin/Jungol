93314b0e914ddfa84417f0bea3f7dd82ec589463
Class01 <Class01@DESKTOP-QKQPSM5>
2023-06-02 14:56:18 +0900

    정올 문자열1 601번문제 선생님풀이 추가!

--- src/q601/Main.java
+++ src/q601/Main.java
@@ -9,17 +9,22 @@ public class Main {
 		String str = sc.nextLine();
 		sc.close();
 
+		String str2 = str + str;
 		for (int i = str.length() - 1; i >= 0; i--) {
-			for (int j = i; j < str.length(); j++) {
-				System.out.print(str.charAt(j));
-			}
-			for (int k = 0; k < i; k++) {
-				System.out.print(str.charAt(k));
-				if (k == i - 1) {
-					System.out.println();
-				}
-			}
+			System.out.println(str2.substring(i, i + str.length()));
 		}
+
+//		for (int i = str.length() - 1; i >= 0; i--) {
+//			for (int j = i; j < str.length(); j++) {
+//				System.out.print(str.charAt(j));
+//			}
+//			for (int k = 0; k < i; k++) {
+//				System.out.print(str.charAt(k));
+//				if (k == i - 1) {
+//					System.out.println();
+//				}
+//			}
+//		}
 	}
 
 }
